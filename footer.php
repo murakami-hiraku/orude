3d71bd5959fa1bcc9706a931ee33fa0b0d5e0fde
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-05 12:07:45 +0900

    footer.phpを作成、基本的な構成を記述

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- footer -->
+<footer class="footer">
+  <!-- footer-info -->
+  <div class="footer-info" id="footer-info">
+  </div>
+  <!-- /footer-info -->
+  <!-- footer-link -->
+  <a class=" footer-link" id="footer-link">
+  </a>
+  <!-- /footer-link -->
+  <small class="footer__copyright">
+  </small>
+</footer>
+<!-- /footer -->
+</div>
+<!-- /wrapper-->
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
