490e59768df144d2107b6595baf617686630e166
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-06 14:57:21 +0900

    フッターコーディング完了

--- footer.php
+++ footer.php
@@ -1,15 +1,24 @@
 <!-- footer -->
 <footer class="footer">
-  <!-- footer-info -->
-  <div class="footer-info" id="footer-info">
+  <div class="footer__inner">
+    <div class="footer__content">
+      <!-- footer-logo -->
+      <a class="footer__logo" href="<?php echo home_url('/'); ?>">
+        <h2>オルデ</h2>
+        <h3>-orude-</h3>
+      </a>
+      <!-- /footer-logo -->
+      <!-- footer-info -->
+      <address class=" footer__info" id="footer-info">
+        <p class="footer__info-address">〒583-0872 <br class="hidden-pc">大阪府羽曳野市はびきの3-316-84</p>
+        <p class="footer__info-tel">TEL 000-000-0000</p>
+        <a href="" class="footer__info-privacy-policy">プライバシーポリシー</a>
+      </address>
+      <!-- /footer-info -->
+    </div>
+    <small class="footer__copyright">©aellede
+    </small>
   </div>
-  <!-- /footer-info -->
-  <!-- footer-link -->
-  <a class=" footer-link" id="footer-link">
-  </a>
-  <!-- /footer-link -->
-  <small class="footer__copyright">
-  </small>
 </footer>
 <!-- /footer -->
 </div>
