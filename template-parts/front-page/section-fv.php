519c373dc97f9358319c2ecbf68bb253cf73f323
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-09 15:06:32 +0900

    fv 表示変更 スワイパーなし

--- template-parts/front-page/section-fv.php
+++ template-parts/front-page/section-fv.php
@@ -11,40 +11,13 @@
         <h2>これからの旅が<br>穏やかな旅でありますように</h2>
       </div>
     </div>
-    <!-- メイン（自動スクロール） -->
-    <div class="swiper fv-swiper">
 
+    <div class="section-fv__img">
+      <img class="img-1" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt="">
+      <img class="img-2" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt="">
+    </div>
 
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <div class="swiper-slide-container">
-            <img class="swiper-img img-1" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt="">
-            <img class="swiper-img img-2" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt="">
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div class="swiper-slide-container">
-            <img class="swiper-img img-1" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt="">
-            <img class="swiper-img img-2" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt="">
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div class="swiper-slide-container">
-            <img class="swiper-img img-1" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt="">
-            <img class="swiper-img img-2" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt="">
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div class="swiper-slide-container">
-            <img class="swiper-img img-1" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt="">
-            <img class="swiper-img img-2" src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt="">
-          </div>
-        </div>
-      </div>
-
-      <!-- 右側：縦型スクロールバー -->
-      <div class="swiper-scrollbar">
-      </div>
+    <div class="section-fv__scrollbar">
 
     </div>
 
