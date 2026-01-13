b1dad833e51af0893653b25a5058fb121c92f904
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-13 11:29:48 +0900

    フロントページ メッセージセクション php骨組み記述

--- front-page.php
+++ front-page.php
@@ -6,6 +6,10 @@
 <?php get_template_part('template-parts/front-page/section-fv'); ?>
 <!-- /ファーストビュー -->
 
+<!-- メッセージ -->
+<?php get_template_part('template-parts/front-page/section-message'); ?>
+<!-- /メッセージ -->
+
 
 <!-- フッター -->
 <?php get_footer(); ?>
---
+++ template-parts/front-page/section-message.php
@@ -0,0 +1,26 @@
+<!-- メッセージ -->
+<section class="section-message">
+  <div class="section-message__inner inner">
+
+    <div class="section-message__img pc">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/message-img-left.png" alt="" class="left">
+    </div>
+
+    <div class="section-message__box">
+      <h2 class="section-message__ttl">僕たちの想い</h2>
+
+      <div class="section-message__txt">
+        <p>どんな時も、共に過ごしてきた、大切な「家族」。</p>
+        <p>そんなかけがえのない存在だからこそ、その旅立ちを、私たちは心を込めてお手伝いしたいと考えています。</p>
+        <p>私たちの使命は、ご家族様の想いに寄り添いながらあたたかなお別れの形をお届けします。</p>
+        <small class="section-message__from">スタッフ一同</small>
+      </div>
+    </div>
+
+    <div class="section-message__img sp">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/message-img-left.png" alt="" class="left">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/message-img-right.png" alt="" class="right">
+    </div>
+  </div>
+</section>
+<!-- /メッセージ -->
