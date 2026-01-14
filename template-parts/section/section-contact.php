227019ab7ff2aba8e4ca92d377a7c836e412c758
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-14 11:32:16 +0900

    共通コンタクトセクション スマホ表示 記述

--- template-parts/section/section-contact.php
+++ template-parts/section/section-contact.php
@@ -1,35 +1,38 @@
 <!-- コンタクト -->
 <section class="section-contact section fadein">
-  <div class="section-contact__inner inner">
 
-    <h2 class="section-contact__ttl">お問い合わせ</h2>
+  <div class="section-contact__wrap">
+    <div class="section-contact__inner inner">
 
-    <div class="section-contact__info">
-      <div class="section-contact__tel"><img src="<?php echo get_template_directory_uri(); ?>/img/tel-icon.svg" alt="">
-        <a href="">0120-000-000</a>
+      <div class="section-contact__info-box">
+        <h1 class="section-contact__ttl">お問い合わせ</h1>
+        <div class="section-contact__info">
+          <div class="section-contact__tel">
+            <a href="">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/tel-icon.svg" alt="">
+              0120-000-000</a>
+          </div>
+          <p>営業時間：9:00~18:00</p>
+          <p>定休日：年中無休(年末年始等を除く)</p>
+        </div>
       </div>
-      <p>営業時間：9:00~18:00</p>
-      <p>定休日：年中無休(年末年始等を除く)</p>
+
+      <div class="section-contact__link-box">
+        <div class="section-contact__links">
+          <a href="" class="section-contact__link mail"><span class="section-contact__link-icon"></span>メールでのお問い合わせ</a>
+          <a href="" class="section-contact__link line"><span class="section-contact__link-icon"></span>LINEでのお問い合わせ</a>
+        </div>
+
+        <p class="section-contact__txt">
+          お支払いは、現金またはクレジットカードでの<br class="hidden-pc">ご精算となります。</p>
+
+        <div class="section-contact__card-logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/contact-card-logo.png" alt="">
+        </div>
+      </div>
+
+      <img src="<?php echo get_template_directory_uri(); ?>/img/contact-img.png" alt="" class="section-contact__img">
     </div>
-
-    <div class="section-contact__links">
-      <a href="" class="section-contact__link mail"></a>
-      <a href="" class="section-contact__link line"></a>
-    </div>
-
-    <p class="section-contact__txt">
-      お支払いは、現金またはクレジットカードでのご精算となります。</p>
-
-    <div class="section-contact__card-logo">
-      <img src="" alt="">
-      <img src="" alt="">
-      <img src="" alt="">
-      <img src="" alt="">
-      <img src="" alt="">
-      <img src="" alt="">
-    </div>
-
-    <img src="" alt="" class="section-contact__img">
 
     <span class="section-contact__bg-txt">-May the journey ahead be a peaceful one.-</span>
 
