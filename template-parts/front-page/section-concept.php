d57f1fcba5e853b5aedcc5a1b9d47eb5388e1e6a
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-16 17:30:53 +0900

    fix section-concept修正

--- template-parts/front-page/section-concept.php
+++ template-parts/front-page/section-concept.php
@@ -1,5 +1,27 @@
 <!-- 私たちの想い（concept） -->
 <section class="section-concept">
 
+  <span class="section-concept__bg-circle bg-circle"></span>
+  <div class="section-concept__inner inner">
+
+    <div class="section-concept__img-left">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/concept-img-left.png" alt="" class="left">
+    </div>
+
+    <div class="section-concept__box">
+      <h2 class="section-concept__ttl">僕たちの想い</h2>
+
+      <div class="section-concept__txt">
+        <p>どんな時も、共に過ごしてきた、大切な「家族」。</p>
+        <p>そんなかけがえのない存在だからこそ、その旅立ちを、私たちは心を込めてお手伝いしたいと考えています。</p>
+        <p>私たちの使命は、ご家族様の想いに寄り添いながらあたたかなお別れの形をお届けします。</p>
+        <small class="section-concept__from">スタッフ一同</small>
+      </div>
+    </div>
+
+    <div class="section-concept__img-right">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/concept-img-right.png" alt="" class="right">
+    </div>
+  </div>
 </section>
 <!-- /私たちの想い（concept） -->
