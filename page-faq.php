38669a69fa1704e775991b6feafee9b4f46f2aa6
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-19 14:00:07 +0900

    feat page-faq.phpファイル追加 骨組み記述

---
+++ page-faq.php
@@ -0,0 +1,22 @@
+<!-- ヘッダー -->
+<?php get_header(); ?>
+<!-- /ヘッダー -->
+
+<main class="page-faq">
+  <?php
+  get_template_part('template-parts/block/block-page-ttl');
+  ?>
+
+  <section class="page-faq__section fadein">
+
+  </section>
+
+  <!-- コンタクト -->
+  <?php get_template_part('template-parts/section/section-contact'); ?>
+  <!-- /コンタクト -->
+
+</main>
+
+<!-- フッター -->
+<?php get_footer(); ?>
+<!-- /フッター -->
