dc206ecb0a271fff1666b69159be56974e7d2318
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-19 14:56:01 +0900

    feat style : page-faq基本構造の記述追加  基本的なスタイル記述

--- page-faq.php
+++ page-faq.php
@@ -9,6 +9,52 @@
 
   <section class="page-faq__section fadein">
 
+    <?php get_template_part('template-parts/parts/parts-bg-circle'); ?>
+
+    <div class="page-faq__inner inner">
+
+      <div class="page-faq__contents">
+        <div class="page-faq__category">
+          <h2 class="page-faq__ttl">プレミアム訪問ペット火葬に関するご質問</h2>
+          <div class="page-faq__card">
+            <div class="page-faq__q">
+              <span class="page-faq__label">Q1</span>
+              <h3 class="page-faq__txt">対応エリアはどこですか？</h3>
+            </div>
+            <div class="page-faq__a">
+              <span class="page-faq__label">A1</span>
+              <p class="page-faq__txt">大阪府全域および周辺エリアで対応しております（大阪市内・北摂・泉州・阪神間など）。隣接府県（例：兵庫県や京都府南部など）もご相談に応じておりますので、まずはお問い合わせください。</p>
+            </div>
+          </div>
+        </div>
+        <div class="page-faq__category">
+          <h2 class="page-faq__ttl">ペット葬儀・セレモニーに関するご質問</h2>
+          <div class="page-faq__card">
+            <div class="page-faq__q">
+              <span class="page-faq__label">Q1</span>
+              <h3 class="page-faq__txt">自宅で葬儀をする場合、何か準備しておくことはありますか？</h3>
+            </div>
+            <div class="page-faq__a">
+              <span class="page-faq__label">A1</span>
+              <p class="page-faq__txt">基本的に特別なご準備は不要です。必要なものはすべて当社でお持ちいたします。ただ、ご自宅セレモニーの場合は、お別れの場としてリビングなど一室をお借りできるようご準備いただけるとスムーズです。また、その子に手向けたいお花や、思い出のお写真などがございましたら、ご用意いただければ祭壇に飾ることもできます。スタッフ到着後に配置などお手伝いいたしますので、慌てずにお待ちください。</p>
+            </div>
+          </div>
+        </div>
+        <div class="page-faq__category">
+          <h2 class="page-faq__ttl">人とペットの永代供養サービスに関するご質問</h2>
+          <div class="page-faq__card">
+            <div class="page-faq__q">
+              <span class="page-faq__label">Q1</span>
+              <h3 class="page-faq__txt">ペットのみでも永代供養を申し込めますか？</h3>
+            </div>
+            <div class="page-faq__a">
+              <span class="page-faq__label">A1</span>
+              <p class="page-faq__txt">はい、ペットちゃん単独のお申込みも可能です。ペットのみの場合は、他のペットたちと一緒に供養する合同供養墓や納骨堂での永代供養をご利用いただけます。実際、ペットだけで行う永代供養では他のペットの遺骨と合同で納骨・供養する形式（合祀型）が選ばれることが多くなっています。飼い主様が後から合流されないケースでも、寺院がペットのご供養を永代にわたり続けますのでご安心ください。もちろん後になって飼い主様側を追加でお申込みいただくことも可能です。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
   </section>
 
   <!-- コンタクト -->
