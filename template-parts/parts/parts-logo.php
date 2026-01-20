57d6b3a77304ca80a1ea0b60c4335e45539d19d6
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-20 13:28:35 +0900

    feat fix : logo画像追加 修正

--- template-parts/parts/parts-logo.php
+++ template-parts/parts/parts-logo.php
@@ -1,4 +1,10 @@
         <a class="parts-logo" href="<?php echo home_url('/'); ?>">
-          <h2>オルデ</h2>
-          <h3>-orude-</h3>
+          <div class="parts-logo__img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/logo-img.png" alt="">
+          </div>
+
+          <div class="parts-logo__word">
+            <h2>オルデ</h2>
+            <h3>-orude-</h3>
+          </div>
         </a>
