b70a56bd74dc2eae6d625f5d48618fd5c9d13532
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-21 11:28:48 +0900

    feat : page-companyファイル作成

---
+++ page-company.php
@@ -0,0 +1,24 @@
+<!-- ヘッダー -->
+<?php get_header(); ?>
+<!-- /ヘッダー -->
+
+<main class="page-company">
+  <?php
+  get_template_part('template-parts/block/block-page-ttl');
+  ?>
+
+  <section class="page-company__section section fadein">
+    <div class="page-company__inner inner">
+
+    </div>
+  </section>
+
+  <!-- コンタクト -->
+  <?php get_template_part('template-parts/section/section-contact'); ?>
+  <!-- /コンタクト -->
+
+</main>
+
+<!-- フッター -->
+<?php get_footer(); ?>
+<!-- /フッター -->
