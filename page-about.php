0187aae04b35bd37486cf45808e4a7129d73beb9
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-21 13:32:04 +0900

    feat refactor : page-about装飾画像部分パーツ化

--- page-about.php
+++ page-about.php
@@ -12,17 +12,19 @@
 
     <div class="page-about__inner inner">
       <div class="page-about__container">
-        <div class="page-about__img img-1">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/bg-flower-1.png" alt="">
-        </div>
+        <?php
+        get_template_part('template-parts/parts/parts-bg-flower--right');
+        ?>
+
         <div class="page-about__box">
           <h2 class="page-about__ttl">代表者ご挨拶</h2>
           <p class="page-about__txt">ペットは私たちの大切な家族だからこそ、最愛の家族を安心して送り出せる場所がいかに大切かを、私たちは実感しています。<br>最後のお別れが悲しみだけではなく、共に過ごしたすばらしい日々を振り返る温かな時間となりますよう、また、皆様がこれからの一歩を踏み出すお手伝いができますよう、スタッフ一同、誠心誠意努めてまいります。</p>
           <p class="page-about__signature">代表取締役　北池　章一</p>
         </div>
-        <div class="page-about__img img-2">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/bg-flower-2.png" alt="">
-        </div>
+        <?php
+        get_template_part('template-parts/parts/parts-bg-flower--left');
+        ?>
+
       </div>
     </div>
 
