b919432b1dd77286b08d3947ecf9a02cde95695c
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-21 16:47:13 +0900

    feat refactor : page-privacy-policyファイル追加 骨組み記述

---
+++ page-privacy-policy.php
@@ -0,0 +1,28 @@
+<!-- ヘッダー -->
+<?php get_header(); ?>
+<!-- /ヘッダー -->
+
+<main class="page-privacy-policy">
+
+  <?php
+  get_template_part('template-parts/block/block-page-ttl');
+  ?>
+
+  <section class="page-privacy-policy__section section fadein">
+
+
+    <div class="page-privacy-policy__inner inner">
+
+    </div>
+
+  </section>
+
+  <!-- コンタクト -->
+  <?php get_template_part('template-parts/section/section-contact'); ?>
+  <!-- /コンタクト -->
+
+</main>
+
+<!-- フッター -->
+<?php get_footer(); ?>
+<!-- /フッター -->
