63815cf1c128b6634c262eba27277bd75538f60f
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-22 12:09:40 +0900

    refactor : scssパス修正

---
+++ template-parts/pages/contact-confirm.php
@@ -0,0 +1,13 @@
+  <!-- ヘッダー -->
+  <?php get_header(); ?>
+  <!-- /ヘッダー -->
+
+  <main class="page-contact-confirm">
+
+
+  </main>
+
+
+  <!-- フッター -->
+  <?php get_footer(); ?>
+  <!-- /フッター -->
---
+++ template-parts/pages/contact-thanks.php
@@ -0,0 +1,13 @@
+  <!-- ヘッダー -->
+  <?php get_header(); ?>
+  <!-- /ヘッダー -->
+
+  <main class="page-contact-thanks">
+
+
+  </main>
+
+
+  <!-- フッター -->
+  <?php get_footer(); ?>
+  <!-- /フッター -->
