79db7fdbbfe1e151a299742dcd5252f6543652c7
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-26 13:50:22 +0900

    fix : パス修正、クラス名修正

--- template-parts/parts/parts-bg-flower--left.php
+++ template-parts/parts/parts-bg-flower--left.php
@@ -1,3 +1,3 @@
-        <div class="parts-bg-flower--left">
+        <div class="parts-bg-flower parts-bg-flower--left">
           <img src="<?php echo get_template_directory_uri(); ?>/img/bg-flower-left.png" alt="">
         </div>
--- template-parts/parts/parts-bg-flower--right.php
+++ template-parts/parts/parts-bg-flower--right.php
@@ -1,3 +1,3 @@
-        <div class="parts-bg-flower--right">
+        <div class="parts-bg-flower parts-bg-flower--right">
           <img src="<?php echo get_template_directory_uri(); ?>/img/bg-flower-right.png" alt="">
         </div>
