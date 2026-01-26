c57460e7071df2e2a3ab99af4b7619a6a5f9d867
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-26 14:49:32 +0900

    feat style : contact-confirm php記述 スマホ表示記述

--- template-parts/pages/contact-confirm.php
+++ template-parts/pages/contact-confirm.php
@@ -4,7 +4,29 @@
 
   <main class="page-contact-confirm">
 
-    <?php the_content(); ?>
+    <div class="page-contact-confirm__section section fadein">
+
+      <div class="page-contact-confirm__inner inner">
+
+        <div class="page-contact-confirm__container">
+          <?php
+          get_template_part('template-parts/parts/parts-bg-flower--white')
+          ?>
+          <h2 class="page-contact-confirm__ttl">お問い合わせ・お申込み</h2>
+
+          <div class="page-contact-confirm__steps">
+            <div class="page-contact-confirm__step page-contact-confirm__step--input">1.入力</div>
+            <div class="page-contact-confirm__step page-contact-confirm__step--confirm">2.確認</div>
+            <div class="page-contact-confirm__step page-contact-confirm__step--complete">3.完了</div>
+          </div>
+
+          <div class="page-contact-confirm__form">
+            <?php the_content(); ?>
+          </div>
+
+        </div>
+      </div>
+    </div>
 
   </main>
 
