267a09d08d7deda64897fef6b899f17086647bfc
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-27 10:43:33 +0900

    refactor : contact-stepsパーツ化

---
+++ template-parts/parts/parts-page-contact-steps.php
@@ -0,0 +1,5 @@
+          <div class="parts-page-contact-steps">
+            <div class="parts-page-contact-steps__step parts-page-contact-steps__step--input">1.入力</div>
+            <div class="parts-page-contact-steps__step parts-page-contact-steps__step--confirm">2.確認</div>
+            <div class="parts-page-contact-steps__step parts-page-contact-steps__step--complete">3.完了</div>
+          </div>
