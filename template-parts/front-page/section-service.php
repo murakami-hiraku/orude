1a307a6d4695cf98c161eec0a4570d80076886f2
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-29 11:40:25 +0900

    style : fp sec service  pc表示記述　phpタグ追加

--- template-parts/front-page/section-service.php
+++ template-parts/front-page/section-service.php
@@ -10,24 +10,24 @@
     <div class="section-service__cards">
 
       <div class="section-service__card">
-        <h3 class="section-service__card-ttl">訪問火葬</h3>
-        <p class="section-service__card-txt">専用設計の火葬炉を備えた専用車でご自宅までお伺いし、ご家族だけで、静かにお見送りができる環境をご用意いたします。<br>慣れ親しんだ場所でゆっくりとお別れの時間を過ごせます。</p>
-        <div class="section-service__img"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-1.png" alt=""></div>
-        <a href="" class="section-service__link section-service__link--cremation">サービス詳細へ</a>
+        <h3 class="section-service__card-ttl section-service__card-item">訪問火葬</h3>
+        <p class="section-service__card-txt section-service__card-item">専用設計の火葬炉を備えた専用車でご自宅までお伺いし、ご家族だけで、静かにお見送りができる環境をご用意いたします。<br>慣れ親しんだ場所でゆっくりとお別れの時間を過ごせます。</p>
+        <div class="section-service__img section-service__card-item"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-1.png" alt=""></div>
+        <a href="" class="section-service__link section-service__card-item section-service__link--cremation">サービス詳細へ</a>
       </div>
 
       <div class="section-service__card">
-        <h3 class="section-service__card-ttl">ご葬儀・セレモニー</h3>
-        <p class="section-service__card-txt">ご家族らしい、かたちにとらわれないお見送りをご提案いたします。<br>想いに寄り添いながらプロデュースいたします。</p>
-        <div class="section-service__img"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-2.png" alt=""></div>
-        <a href="" class="section-service__link section-service__link--ceremony">セレモニーの詳細へ</a>
+        <h3 class="section-service__card-ttl section-service__card-item">ご葬儀・セレモニー</h3>
+        <p class="section-service__card-txt section-service__card-item">ご家族らしい、かたちにとらわれないお見送りをご提案いたします。<br>想いに寄り添いながらプロデュースいたします。</p>
+        <div class="section-service__img section-service__card-item"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-2.png" alt=""></div>
+        <a href="" class="section-service__link section-service__card-item  section-service__link--ceremony">セレモニーの詳細へ</a>
       </div>
 
       <div class="section-service__card">
-        <h3 class="section-service__card-ttl">永代供養</h3>
-        <p class="section-service__card-txt">お子様のご遺骨を永代に渡り供養いたします。<br>亡くなったお子様とご家族様が”語り合える場所”をご案内いたします。</p>
-        <div class="section-service__img"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-3.png" alt=""></div>
-        <a href="" class="section-service__link section-service__link--perpetual-care">永代供養の詳細へ</a>
+        <h3 class="section-service__card-ttl section-service__card-item">永代供養</h3>
+        <p class="section-service__card-txt section-service__card-item">お子様のご遺骨を永代に渡り供養いたします。<br>亡くなったお子様とご家族様が”語り合える場所”をご案内いたします。</p>
+        <div class="section-service__img section-service__card-item"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-3.png" alt=""></div>
+        <a href="" class="section-service__link section-service__card-item section-service__link--perpetual-care">永代供養の詳細へ</a>
       </div>
 
     </div>
