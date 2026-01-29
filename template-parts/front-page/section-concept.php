7d8ac6f70f61d4b1004291d14c8a176cd4224df4
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-29 10:35:52 +0900

    style : front-page section-concept  pc表示記述

--- template-parts/front-page/section-concept.php
+++ template-parts/front-page/section-concept.php
@@ -7,22 +7,22 @@
 
   <div class="section-concept__inner inner">
 
-    <div class="section-concept__img left">
+    <div class="section-concept__img section-concept__img--left">
       <img src="<?php echo get_template_directory_uri(); ?>/img/concept-img-left.png" alt="" class="left">
     </div>
 
     <div class="section-concept__box">
-      <h2 class="section-concept__ttl">僕たちの想い</h2>
+      <h2 class="section-concept__ttl">私たちの想い</h2>
 
       <div class="section-concept__txt">
         <p>どんな時も、共に過ごしてきた、大切な「家族」。</p>
-        <p>そんなかけがえのない存在だからこそ、その旅立ちを、私たちは心を込めてお手伝いしたいと考えています。</p><br>
+        <p>そんなかけがえのない存在だからこそ、<br>その旅立ちを、私たちは心を込めてお手伝いしたいと考えています。</p><br>
         <p>私たちの使命は、<br>ご家族様の想いに寄り添いながらあたたかなお別れの形をお届けします。</p>
       </div>
       <small class="section-concept__from">スタッフ一同</small>
     </div>
 
-    <div class="section-concept__img right">
+    <div class="section-concept__img section-concept__img--right">
       <img src="<?php echo get_template_directory_uri(); ?>/img/concept-img-right.png" alt="" class="right">
     </div>
   </div>
