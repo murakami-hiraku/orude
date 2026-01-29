205a6ef49d63f828a1b3af46dc09d7cf0a2136f1
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-29 13:36:34 +0900

    feat : fp sec guide  php基本構造記述

--- template-parts/front-page/section-guide.php
+++ template-parts/front-page/section-guide.php
@@ -1,5 +1,21 @@
-<!-- ご利用の流れ（guide） -->
-<section class="section-guide">
+<!-- ペットちゃんがお亡くなりになったら（guide） -->
+<section class="section-guide section">
+
+  <div class="section-guide__inner inner">
+
+    <h2 class="section-guide__ttl">ペットちゃんがお亡くなりになったら</h2>
+
+    <div class="section-guide__content">
+      <div class="section-guide__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/concept-img-left.png" alt="">
+      </div>
+
+      <div class="section-guide__body">
+        <p class="section-guide__txt">「その時」が訪れたとき、どうすればいいのか分からず戸惑ってしまう方も少なくありません。 大切なご家族を安らかに送り出すために、すぐにできること、してあげたいことをわかりやすくご案内しています。</p>
+        <a class="section-guide__link" href="">詳しく見る</a>
+      </div>
+    </div>
+  </div>
 
 </section>
-<!-- /ご利用の流れ（guide） -guid
+<!-- /ペットちゃんがお亡くなりになったら（guide） -->
