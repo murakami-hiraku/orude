fb5bf042a7b1f8668db71509b35b7e8fd9bd67e7
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-29 11:09:29 +0900

    feat : front-page section-service  php基本構造記述

--- template-parts/front-page/section-service.php
+++ template-parts/front-page/section-service.php
@@ -1,5 +1,38 @@
 <!-- 大切な家族との、最後の時間を穏やかに過ごしていただくために（service） -->
-<section class="section-service">
+<section class="section-service section">
+
+  <div class="section-service__inner inner">
+
+    <h2 class="section-service__ttl">
+      大切な家族との、最後の時間を穏やかに過ごしていただくために
+    </h2>
+
+    <div class="section-service__cards">
+
+      <div class="section-service__card">
+        <h3 class="section-service__card-ttl">訪問火葬</h3>
+        <p class="section-service__card-txt">専用設計の火葬炉を備えた専用車でご自宅までお伺いし、ご家族だけで、静かにお見送りができる環境をご用意いたします。<br>慣れ親しんだ場所でゆっくりとお別れの時間を過ごせます。</p>
+        <div class="section-service__img"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-1.png" alt=""></div>
+        <a href="" class="section-service__link section-service__link--cremation">サービス詳細へ</a>
+      </div>
+
+      <div class="section-service__card">
+        <h3 class="section-service__card-ttl">ご葬儀・セレモニー</h3>
+        <p class="section-service__card-txt">ご家族らしい、かたちにとらわれないお見送りをご提案いたします。<br>想いに寄り添いながらプロデュースいたします。</p>
+        <div class="section-service__img"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-2.png" alt=""></div>
+        <a href="" class="section-service__link section-service__link--ceremony">セレモニーの詳細へ</a>
+      </div>
+
+      <div class="section-service__card">
+        <h3 class="section-service__card-ttl">永代供養</h3>
+        <p class="section-service__card-txt">お子様のご遺骨を永代に渡り供養いたします。<br>亡くなったお子様とご家族様が”語り合える場所”をご案内いたします。</p>
+        <div class="section-service__img"><img src="<?php echo get_template_directory_uri(); ?>/img/service-img-3.png" alt=""></div>
+        <a href="" class="section-service__link section-service__link--perpetual-care">永代供養の詳細へ</a>
+      </div>
+
+    </div>
+
+  </div>
 
 </section>
 <!-- /大切な家族との、最後の時間を穏やかに過ごしていただくために（service） -->
