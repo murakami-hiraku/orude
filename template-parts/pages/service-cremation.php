80f73722a7da851578aa334e9bb079c9ed3cc2da
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-30 14:23:57 +0900

    feat : page-service-cremation基本構造記述途中　大枠のみ記述　中身はこれから

--- template-parts/pages/service-cremation.php
+++ template-parts/pages/service-cremation.php
@@ -2,7 +2,169 @@
   <?php get_header(); ?>
   <!-- /ヘッダー -->
 
-  <main class="">
+  <main class="page-s-cremation">
+    <?php
+    get_template_part('template-parts/block/block-pate-ttl')
+    ?>
+    <section class="page-s-cremation__section page-s-cremation__section--1 section fadein">
+
+      <div class="page-s-cremation__inner inner">
+        <?php
+        get_template_part('template-parts/parts/parts-bg-circle');
+        ?>
+
+        <h2 class="page-s-cremation__ttl">ご自宅近くで行う、やさしいお見送り</h2>
+        <p class="page-s-cremation__txt">オルデの訪問火葬は、ご自宅またはご自宅近隣まで専用車でお伺いし、火葬のみを執り行うサービスです。葬儀やセレモニーは行わず、「静かに、丁寧に火葬をしてほい」「形式よりも負担を少なくしたい」というご家族様のためのプランです。慣れ親しんだ場所の近くでお見送りができるため、移動のご負担もなく、落ち着いた気持ちで最期の時間をお過ごしいただけます。</p>
+      </div>
+
+    </section>
+
+    <section class="page-s-cremation__section page-s-cremation__section--2 section fadein">
+      <div class="page-s-cremation__inner inner">
+        <h2 class="page-s-cremation__ttl">アエル・デならではの安心品質</h2>
+        <p class="page-s-cremation__txt">大切な家族の旅立ちを真心を込めてお手伝いいたします。</p>
+
+        <div class="page-s-cremation__cards">
+          <div class="page-s-cremation__card">
+
+            <div class="page-s-cremation__card-img">
+              <img src="" alt="">
+            </div>
+            <h2 class="page-s-cremation__card-ttl"></h2>
+
+            <p class="page-s-cremation__card-txt"></p>
+
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+    <section class="page-s-cremation__section page-s-cremation__section--3 section fadein">
+
+      <div class="page-s-cremation__inner inner">
+        <h2 class="page-s-cremation__ttl">火葬後について</h2>
+        <div class="page-s-cremation__img">
+          <img src="" alt="">
+        </div>
+        <p class="page-s-cremation__txt">火葬後は、ご希望に応じてお骨上げを行い、ご遺骨を骨壷にお納めしてご返骨いたします。 ご自宅でのご供養や、その後のご相談についても承っております。骨壷・骨袋（覆い袋）は料金に含まれております。 分骨カプセルやメモリアルグッズなどをご希望の場合は、事前にご案内いたします。</p>
+      </div>
+
+    </section>
+
+    <!-- コンタクト -->
+    <?php get_template_part('template-parts/section/section-contact'); ?>
+    <!-- /コンタクト -->
+
+
+    <section class="page-s-cremation__section page-s-cremation__section--4 section fadein">
+
+      <div class="page-s-cremation__inner inner">
+
+        <?php
+        get_template_part('template-parts/parts/parts-bg-flower--right');
+        ?>
+        <h2 class="page-s-cremation__ttl">プランについて</h2>
+        <div class="page-s-cremation__img">
+          <img src="" alt="">
+        </div>
+
+        <div class="page-s-cremation__cards">
+          <div class="page-s-cremation__card">
+            <h2 class="page-s-cremation__card-ttl"></h2>
+            <p class="page-s-cremation__card-txt"></p>
+            <div class="page-s-cremation__flow">
+              <div class="page-s-cremation__flow-container">
+                <div class="page-s-cremation__flow-step page-s-cremation__flow-step--1">
+                </div>
+              </div>
+            </div>
+            <div class="page-s-cremation__card-img">
+              <img src="" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="page-s-cremation__section page-s-cremation__section--5 section fadein">
+
+      <div class="page-s-cremation__inner inner">
+        <?php
+        get_template_part('template-parts/parts/parts-bg-flower--right');
+        ?>
+
+        <h2 class="page-s-cremation__ttl">お見送りの流れ</h2>
+
+        <div class="page-s-cremation__cards">
+          <div class="page-s-cremation__card">
+            <div class="page-s-cremation__card-img">
+              <img src="" alt="">
+            </div>
+            <h2 class="page-s-cremation__card-ttl"></h2>
+            <p class="page-s-cremation__card-txt"></p>
+          </div>
+        </div>
+
+        <?php
+        get_template_part('template-parts/parts/parts-bg-flower--left');
+        ?>
+
+      </div>
+
+    </section>
+
+    <section class="page-s-cremation__section page-s-cremation__section--6 section fadein">
+
+      <div class="page-s-cremation__inner inner">
+
+        <h2 class="page-s-cremation__ttl">料金プラン（目安）</h2>
+        <p class="page-s-cremation__txt">お迎えの手配からお別れの式、そしてご供養まで—— すべての工程を、私たちが責任を持ってお手伝いさせていただきます。</p>
+        <p class="page-s-cremation__txt sub">下記は目安となる税込料金です。正式なお見積りはお問い合わせ時にご案内いたします。</p>
+
+        <div class="page-s-cremation__container">
+          <div class="page-s-cremation__img">
+            <img src="" alt="">
+          </div>
+
+          <div class="page-s-cremation__cards">
+            <div class="page-s-cremation__card">
+              <h2 class="page-s-cremation__card-ttl">料金に含まれる内容</h2>
+              <p class="page-s-cremation__card-txt">個別葬プラン（一任個別葬・立会個別葬）には、骨壷・骨袋（覆い袋）が含まれております。合同葬プランは、お骨をお返しできません。火葬後は、一部のお骨を合同墓へお納めいたします。生花・祭壇・葬儀セレモニー等は含まれておりません。</p>
+            </div>
+            <div class="page-s-cremation__card">
+              <h2 class="page-s-cremation__card-ttl">お迎え・出張費について</h2>
+              <p class="page-s-cremation__card-txt">ご遺体のお迎えは大阪市内無料大阪市外3,300円（高速代をご請求することもございます。状況によりご相談を承っております。</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+
+
+    <!-- コンタクト -->
+    <?php get_template_part('template-parts/section/section-contact'); ?>
+    <!-- /コンタクト -->
+
+    <section class="page-s-cremation__section page-s-cremation__section--7 section fadein">
+
+      <div class="page-s-cremation__inner inner">
+        <h2 class="page-s-cremation__ttl">よくあるご質問（FAQ）</h2>
+        <div class="page-s-cremation__content"></div>
+      </div>
+
+    </section>
+
+    <!-- コンタクト -->
+    <?php get_template_part('template-parts/section/section-contact'); ?>
+    <!-- /コンタクト -->
+
+
 
   </main>
 
