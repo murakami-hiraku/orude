9f890215483756774e8a343b8b75d00f2e82b1f2
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-01-30 11:08:42 +0900

    feat : ラインリンクボタン追加

--- template-parts/pages/contact-thanks.php
+++ template-parts/pages/contact-thanks.php
@@ -24,6 +24,11 @@
           ?>
           <p class="page-contact page-contact-thanks__txt">お問い合わせいただき誠にありがとうございます<br>営業担当のものから追って、ご連絡いたしますので、<br>少々お時間いただけますと幸いです。</p>
         </div>
+
+        <div class="page-contact-thanks__link">
+          <p class="page-contact-thanks__link-txt">分からないことは、公式LINEで気軽にご相談ください。<br class="br-1">状況に合わせて、必要な手順をご案内します。<br class="br-2">友だち追加はこちら。</p>
+          <a href="" class="section-contact__link line"><span class="link-icon"></span>LINEでのお問い合わせ</a>
+        </div>
       </div>
     </div>
 
