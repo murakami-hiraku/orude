c62e43bd7fb90c8fed49487e6c74bd56e5cfd1fa
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-02-04 13:20:15 +0900

    fix : クラス名修正

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.mySwiper', {
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
     loop: true, // ループさせる
     slidesPerView: 1, // 表示枚数
     spaceBetween: 20, // スライド間隔
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 });
