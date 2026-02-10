6d00bbbfafca22dacd6dec3ac9b9002f576641d9
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-02-10 17:11:16 +0900

    fix : fv修正完了

--- template-parts/front-page/section-fv.php
+++ template-parts/front-page/section-fv.php
@@ -41,33 +41,44 @@
       </div>
 
       <div class="p-fv-windows">
-        <div class="p-fv-slider p-fv-slider--1">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-3.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-4.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-5.png" alt=""></div>
+        <div class="p-fv-slider-wrap p-fv-slider-wrap--1">
+          <div class="p-fv-slider-circle p-fv-slider-circle--1"></div>
+          <div class="p-fv-slider p-fv-slider--1">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-3.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-4.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-5.png" alt=""></div>
+            </div>
           </div>
         </div>
 
-        <div class="p-fv-slider p-fv-slider--2">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-3.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-4.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-5.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt=""></div>
+        <div class="p-fv-slider-wrap p-fv-slider-wrap--2">
+          <div class="p-fv-slider-circle p-fv-slider-circle--2"></div>
+
+          <div class="p-fv-slider p-fv-slider--2">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-3.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-4.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-5.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt=""></div>
+            </div>
           </div>
         </div>
 
-        <div class="p-fv-slider p-fv-slider--3">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-3.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-4.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-5.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt=""></div>
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt=""></div>
+        <div class="p-fv-slider-wrap p-fv-slider-wrap--3">
+          <div class="p-fv-slider-circle p-fv-slider-circle--3"></div>
+
+          <div class="p-fv-slider p-fv-slider--3">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-3.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-4.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-5.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-1.png" alt=""></div>
+              <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/fv-img-2.png" alt=""></div>
+            </div>
           </div>
         </div>
       </div>
