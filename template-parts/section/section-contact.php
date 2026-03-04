2a4a2d725acc5fe6e12588950f171a8180447927
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-03-04 11:29:51 +0900

    fix : secコンタクトスタイル修正

--- template-parts/section/section-contact.php
+++ template-parts/section/section-contact.php
@@ -29,11 +29,10 @@
           ]); ?>
         </div>
         <p class="section-contact__txt">
-          お支払いは、現金またはクレジットカードでの<br class="hidden-tb">ご精算となります。</p>
+          お支払いは、現金またはクレジットカードでのご精算となります。<br>
+          ※永代合同墓のご契約は現金のみとなります。
+        </p>
 
-        <div class="section-contact__card-logo">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/ui/contact-card-logo.png" alt="">
-        </div>
 
       </div>
 
