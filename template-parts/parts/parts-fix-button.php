f933a94666d5e24c97506da66d74f01bb079c4b3
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-05-13 11:31:37 +0900

    fix: フォーマット

--- template-parts/parts/parts-fix-button.php
+++ template-parts/parts/parts-fix-button.php
@@ -1,16 +1,16 @@
-     <div class="parts-fix-button">
+<div class="parts-fix-button">
 
-       <a class="parts-fix-button__link cremation" href="<?php echo home_url('/service-cremation'); ?>">
-         <p class="link-txt">
-           火葬ご希望の方はこちら</p>
-       </a>
+  <a class="parts-fix-button__link cremation" href="<?php echo home_url('/service-cremation'); ?>">
+    <p class="link-txt">
+      火葬ご希望の方はこちら</p>
+  </a>
 
-       <a class="parts-fix-button__link ceremony" href="<?php echo home_url('/service-ceremony'); ?>">
-         <p class="link-txt">自宅葬をご希望の方はこちら</p>
-       </a>
+  <a class="parts-fix-button__link ceremony" href="<?php echo home_url('/service-ceremony'); ?>">
+    <p class="link-txt">自宅葬をご希望の方はこちら</p>
+  </a>
 
-       <a class="parts-fix-button__link perpetual-care" href="<?php echo home_url('/service-perpetual-care'); ?>">
-         <p class="link-txt">納骨をご希望の方はこちら</p>
-       </a>
+  <a class="parts-fix-button__link perpetual-care" href="<?php echo home_url('/service-perpetual-care'); ?>">
+    <p class="link-txt">納骨をご希望の方はこちら</p>
+  </a>
 
-     </div>
+</div>
