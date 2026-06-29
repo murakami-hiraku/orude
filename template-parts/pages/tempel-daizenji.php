78fc611d4e8c64f7b9b2eccd975cb123ea57821d
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-06-29 14:42:42 +0900

    feat: 各寺のページを追加するためファイルの準備

---
+++ template-parts/pages/tempel-daizenji.php
@@ -0,0 +1,25 @@
+<!-- ヘッダー -->
+<?php get_header(); ?>
+<!-- /ヘッダー -->
+
+<main class="page-tempel">
+  <?php
+  get_template_part('template-parts/block/block-page-ttl');
+  ?>
+
+  <section class="raikouji section fadein">
+
+
+
+
+  </section>
+
+  <!-- コンタクト -->
+  <?php get_template_part('template-parts/section/section-contact'); ?>
+  <!-- /コンタクト -->
+
+</main>
+
+<!-- フッター -->
+<?php get_footer(); ?>
+<!-- /フッター -->
