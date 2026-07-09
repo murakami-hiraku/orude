b46689959a2d2bc59e28f1268470373564071140
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-07-09 15:44:16 +0900

    feat: お知らせセクション骨組み記述完了

--- template-parts/section/section-news-image.php
+++ template-parts/section/section-news-image.php
@@ -1,14 +1,52 @@
 <!-- 画像付きお知らせ -->
+<?php
+$args = array(
+  'post_type' => 'post',
+  'posts_per_page' => 3,
+  'orderby' => 'date',
+  'order' => 'DESC',
+);
+$news_query = new WP_Query($args);
+?>
+
 <section class="section-news section fadein">
+  <h2 class="section-news__ttl">お知らせ</h2>
 
-  <div class="section-news__wrap">
+  <?php if ($news_query->have_posts()): ?>
+    <ul class="section-news__list-thumb">
+      <?php while ($news_query->have_posts()):
+        $news_query->the_post(); ?>
+        <li class="section-news__item">
+          <a href="<?php the_permalink(); ?>">
 
-    <div class="section-news__inner inner">
+            <div class="section-news__thumb-wrap">
+              <div class="section-news__thumb-bg"></div>
+              <div class="section-news__thumb-img">
+                <?php if (has_post_thumbnail()): ?>
+                  <?php the_post_thumbnail('medium'); // 中サイズで取得 ?>
+                <?php else: ?>
+                  <img src="<?php echo get_theme_file_uri('/images/no-image.jpg'); ?>" alt="No Image">
+                <?php endif; ?>
+              </div>
+            </div>
 
+            <div class="section-news__content">
+              <span class="section-news__date"><?php echo get_the_date('Y.m.d'); ?></span>
+              <p class="section-news__text">
+                <?php
+                $content = wp_strip_all_tags(get_the_content());
+                echo wp_html_excerpt($content, 80, '...'); // 画像付きなので文字数を少し多め（80文字）に調整
+                ?>
+              </p>
+            </div>
 
+          </a>
+        </li>
+      <?php endwhile; ?>
+    </ul>
+  <?php else: ?>
+    <p>現在お知らせはありません。</p>
+  <?php endif; ?>
 
-    </div>
-  </div>
-
-</section>
-<!-- /画像付きお知らせ -->
+  <?php wp_reset_postdata(); ?>
+</section><!-- /画像付きお知らせ -->
